d6b307d7215a70dce8b1939fd07ff3a508c0edf1
Tom <linecontroltran@gmail.com>
2023-10-16 10:11:44 -0400

    numbers, various number operators. version 17

--- 5.numbers.js
+++ 5.numbers.js
@@ -5,4 +5,26 @@ const pi = 3.14;
 
 // Math Operators +, -, *, /, **, %
 
-console.log(10 / 2);
+//console.log(10 / 2);
+//let result = radius % 3;
+//let result = pi * radius ** 2; //radius to the power of 2
+
+//console.log(result);
+
+let likes = 10;
+
+//likes++;
+//likes--;
+
+//likes += 10; // 20
+//likes -= 5; // 5
+//likes *= 2; // 20
+//likes /= 2; // 5
+
+//NaN = not a number
+
+//console.log(5 / "hello"); // NaN
+//console.log(5 * "hello"); // NaN
+
+let result = "the blog has " + likes + " likes";
+console.log(result);
