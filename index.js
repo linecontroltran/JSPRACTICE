b5bf8b3b940d9e4b4154367fabed444203fb5563
Tom <linecontroltran@gmail.com>
2023-10-16 00:02:36 -0400

    tested the var variable. nothing loads below an error if an error show up. version 9

--- index.js
+++ index.js
@@ -14,5 +14,8 @@ age = 40;
 console.log(age);
 
 const points = 100;
-points = 50;
+//points = 50; if there is an error in the code, it wont load anything below
 console.log(points);
+
+var score = 75;
+console.log(score);
