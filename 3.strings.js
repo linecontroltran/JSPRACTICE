38ccad1f009c2b02ad5591a19cab998f08fada7c
Tom <linecontroltran@gmail.com>
2023-10-16 00:13:35 -0400

    Concatention, strings test 2. version 12

--- 3.strings.js
+++ 3.strings.js
@@ -1,4 +1,19 @@
+// Strings
+
 console.log("Hello world");
 
 let email = "linecontroltran@gmail.com";
 console.log(email);
+
+// Concatenation
+
+let firstName = "John";
+let lastName = "Doe";
+
+console.log(firstName, lastName);
+
+let fullName = firstName + lastName;
+console.log(fullName);
+
+let fullName2 = firstName + " " + lastName;
+console.log(fullName2);
