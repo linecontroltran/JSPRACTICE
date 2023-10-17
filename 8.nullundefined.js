53b744ba870839cace7d17faa0e56891c72fe2bb
Tom <linecontroltran@gmail.com>
2023-10-16 23:07:13 -0400

    null undefined, version  20

---
+++ 8.nullundefined.js
@@ -0,0 +1,5 @@
+let age = null; // undefined because no value here, so set it to null
+
+console.log(age, age + 3, `the age is ${age}`);
+
+// why use null? example is just to use as a placeholder if you have an input form with nothing in it i guess
