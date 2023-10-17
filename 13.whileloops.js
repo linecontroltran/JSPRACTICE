806377d9d68d726bb06f8250b4e9898781edb1c2
Tom <linecontroltran@gmail.com>
2023-10-17 12:23:16 -0400

    while loops stuff, version 25

---
+++ 13.whileloops.js
@@ -0,0 +1,28 @@
+// while loops
+
+//for (let i = 0; i < 5; i++) {
+//console.log("in loop:", i);
+//}
+
+//let i = 0;
+
+//while (i < 5) {
+//console.log("in loop: ", i);
+//}
+//this is an infinite loop
+
+const names = ["shaun", "mario", "luigi"];
+
+//let i = 0;
+
+//while (i < 5) {
+//console.log("in loop: ", i);
+//i++;
+//}
+
+let i = 0;
+
+while (i < names.length) {
+  console.log(names[i]);
+  i++;
+}
