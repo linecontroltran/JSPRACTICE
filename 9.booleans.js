03074a7790de007b597bd9f82e0669e6d21ae844
Tom <linecontroltran@gmail.com>
2023-10-16 23:33:10 -0400

    true and false/booleans, version 21

---
+++ 9.booleans.js
@@ -0,0 +1,33 @@
+// booleans and comparisons
+console.log(true, false, "true", "false");
+
+//methods can return booleans
+let email = "luigi@thenetninja.co.uk";
+let names = ["mario", "luigi", "toad"];
+
+//let result = email.includes("@");
+//let result = email.includes("!");
+//let result = names.includes("luigi");
+//let result = names.includes("bowser");
+//console.log(result);
+
+//comparison operators
+
+let age = 25;
+
+//console.log(age == 25);
+//console.log(age == 30);
+//console.log(age != 25);
+//console.log(age != 30);
+//console.log(age > 20);
+//console.log(age < 20);
+//console.log(age <= 20);
+//console.log(age >= 20);
+
+let name = "shaun";
+
+console.log(name == "shaun");
+console.log(name == "Shaun");
+console.log(name > "crystal");
+console.log(name > "Shaun");
+console.log(name > "Crystal");
