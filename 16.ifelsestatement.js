dbb23b0920a5d9f2d9407b92e3a37e5736339ef1
Tom <linecontroltran@gmail.com>
2023-10-18 04:03:04 -0400

    logical NOT operator, version 29

--- 16.ifelsestatement.js
+++ 16.ifelsestatement.js
@@ -1,13 +1,10 @@
 // else if statements
 
-const password = "password";
+const password = "p@ssword1234";
 
-if (password.length >= 12 && password.includes("@")) {
+if (password.length >= 12) {
   console.log("that password is mighty strong");
-} else if (
-  password.length >= 8 ||
-  (password.includes("@") && password.length > 5)
-) {
+} else if (password.length >= 8) {
   console.log("that password is long enough!");
 } else {
   console.log("password is not long enough!");
