54722b3bcd5e428d0f6c7cf4b5c2e3b2def8b960
Tom <linecontroltran@gmail.com>
2023-10-18 02:48:39 -0400

    if ELSE statements, version 28

---
+++ 16.ifelsestatement.js
@@ -0,0 +1,14 @@
+// else if statements
+
+const password = "password";
+
+if (password.length >= 12 && password.includes("@")) {
+  console.log("that password is mighty strong");
+} else if (
+  password.length >= 8 ||
+  (password.includes("@") && password.length > 5)
+) {
+  console.log("that password is long enough!");
+} else {
+  console.log("password is not long enough!");
+}
