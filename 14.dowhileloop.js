8c8cc3c1c8c5e2afcc3da84f8055031d2537de9a
Tom <linecontroltran@gmail.com>
2023-10-18 02:29:07 -0400

    do while loops stuff, version 26

--- 14.dowhileloop.js
+++ 14.dowhileloop.js
@@ -1,1 +1,13 @@
 // do while loops
+
+//let i = 0;
+//while (i < 5) {
+//console.log("val of i is: ", i);
+//i++;
+//}
+
+let i = 4;
+do {
+  console.log("val of i is: ", i);
+  i++;
+} while (i < 5);
