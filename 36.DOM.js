246a317e940ac09fe37e8fb8b3cb1a9c684c8b4e
Tom <linecontroltran@gmail.com>
2023-10-26 18:03:19 -0400

    DOM queryselector, and queryselectorall, version 44

---
+++ 36.DOM.js
@@ -0,0 +1,23 @@
+//const para = document.querySelector("p");
+
+//console.log(para);
+
+//const para = document.querySelector(".error");
+
+//console.log(para);
+
+//const para = document.querySelector("div.error");
+
+//console.log(para);
+
+//const paras = document.querySelectorAll("p");
+//const errors = document.querySelectorAll(".error");
+//paras.forEach((para) => {
+//console.log(para);
+//});
+//console.log(paras);
+
+const paras = document.querySelectorAll("p");
+const errors = document.querySelectorAll(".error");
+
+console.log(errors);
