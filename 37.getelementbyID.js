c3e42afb977ca8b00bcc00ce0fb374bd6dbc7815
Tom <linecontroltran@gmail.com>
2023-10-26 21:28:33 -0400

    getelementbyID, version 45

---
+++ 37.getelementbyID.js
@@ -0,0 +1,20 @@
+// get an element by ID
+//const title = document.getElementById("page-title");
+
+//console.log(title);
+
+// get elements by their class name
+
+//const errors = document.getElementsByClassName("error");
+
+//console.log(errors);
+//console.log(errors[0]);
+//errors.forEach((error) => {
+//console.log(error);
+//}); cannot do this method
+
+//get elements by their tag name
+
+const paras = document.getElementsByTagName("p");
+console.log(paras);
+console.log(paras[1]);
